7591785816ed9ee67758c93289103916095c4470
Touhid-sezan <mdtouhid3131@gmail.com>
2020-12-31 23:06:18 +0600

    db user updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
-const uri = `mongodb+srv://newYearAdmin:${process.env.DB_PASS}@cluster0.sunbd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sunbd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
 const app = express()
